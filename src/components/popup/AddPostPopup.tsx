ed86853a949b2104cf84a82805253250abde7dc7
Oleksandr Kartashev <alexander.kartashev@mvfglobal.com>
2024-12-12 15:46:08 +0200

    fixed popup height

--- src/components/popup/AddPostPopup.tsx
+++ src/components/popup/AddPostPopup.tsx
@@ -40,7 +40,7 @@ const AddPost: React.FC<PostAddPopupProps> = ({
     <>
       <div
         aria-hidden="true"
-        className={`popup popup--edit ${popupVisibleClass} bg-black bg-opacity-30 dark:bg-white dark:bg-opacity-30 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+        className={`popup popup--edit ${popupVisibleClass} bg-black bg-opacity-30 dark:bg-white dark:bg-opacity-30 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 max-h-full`}
       >
         <div className="relative p-4 w-full max-w-2xl max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-black">
